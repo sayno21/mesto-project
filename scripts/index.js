825e394fc75cac83ad17ae62f32e7299f6f8f900
say_no <dallas2112@yandex.ru>
2022-07-13 18:50:10 +0300

    first working popup

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,10 @@ const popupTypeProfile = document.querySelector('.popup_type_profile');
 const popupEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close-button');
 function popupOpen () {
-  popupTypeProfile.classlist.add('.popup_opened');
+  popupTypeProfile.classList.add('popup_opened')
 }
-popupOpen (popupTypeProfile);
 popupEditButton.addEventListener('click', popupOpen);
+function popupClose () {
+  popupTypeProfile.classList.remove('popup_opened')
+}
+popupCloseButton.addEventListener('click', popupClose);

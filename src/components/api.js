f72cc1c159c0bf3d4f166f54667054c2f2de705b
say_no <dallas2112@yandex.ru>
2022-12-14 16:06:50 +0300

    feat: load new profile info from user and display it

--- src/components/api.js
+++ src/components/api.js
@@ -1,48 +1,40 @@
 import {checkRes} from './utilits';
-import {addCardsFromArray, profileTitle, profileSubtitle} from './index';
-
 
 //Загрузка информации о пользователе с сервера
-fetch('https://nomoreparties.co/v1/plus-cohort-17/users/me', {
+export const getProfileInfo = () => {
+  return fetch('https://nomoreparties.co/v1/plus-cohort-17/users/me', {
   headers: {
     authorization: 'c977941d-c8c4-4145-ba23-754c541f6927'
   }
 })
   .then((res) => checkRes(res))
-  .then((res) => {
-    renderResult(profileTitle, res.name);
-    renderResult(profileSubtitle, res.about);
-    renderAvatar(res.avatar);
+}
+
+
+//Загрузка карточек с сервера
+export const loadCardsFromServer = () => {
+  return fetch('https://nomoreparties.co/v1/plus-cohort-17/cards', {
+  headers: {
+    authorization: 'c977941d-c8c4-4145-ba23-754c541f6927'
+  }
+})
+  .then((res) => checkRes(res))
+}
+
+
+//Отправка данных пользователя
+export const sendProfileInfo = (data) => {
+  return fetch('https://nomoreparties.co/v1/plus-cohort-17/users/me', {
+    method: 'PATCH',
+    headers: {
+      authorization: 'c977941d-c8c4-4145-ba23-754c541f6927',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: data.name,
+      about: data.about
+    })
   })
-  .catch((err) => {
-    console.log(`Ошибка: ${err}`);
-    renderError(profileTitle, `Ошибка: ${err}`);
-    renderError(profileSubtitle, `Ошибка: ${err}`);
-  });
-
-  function renderResult(title, text) {
-    title.textContent = text;
-  }
-
-  function renderError(title, err) {
-    title.textContent = err;
-  }
-
-  function renderAvatar(text) {
-    const avatar = document.querySelector('.profile__image');
-    avatar.src = text;
-  }
-
-
-//загрузка карточек с сервера
-fetch('https://nomoreparties.co/v1/plus-cohort-17/cards', {
-  headers: {
-    authorization: 'c977941d-c8c4-4145-ba23-754c541f6927'
-  }
-})
-  .then((res) => checkRes(res))
-  .then((res) => {
-    console.log(res);
-    addCardsFromArray(res);
-  });
+    .then((res) => checkRes(res))
+}
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 import '../index.css';
-import {initialCards, popupTypeProfile, popupTypeNewcard, popupTypeZoom, elementContainer, imageTitle, imageLink, newCard} from './constants';
+import {popupTypeProfile, popupTypeNewcard, popupTypeZoom, elementContainer, imageTitle, imageLink, newCard} from './constants';
 import {enableValidation} from './validate';
 import {closePopupOverlay, openPopup, closePopup} from './modal';
 import {addCards} from './card';
-import {foo} from './api'
+import {getProfileInfo, loadCardsFromServer, sendProfileInfo} from './api';
 
 //Добавление новой карточки по сабмиту
 function addNewElement (evt) {
@@ -14,18 +14,18 @@ function addNewElement (evt) {
 newCard.addEventListener('submit', addNewElement);
 
 //Редактирование профиля
-const formTypeProfile = document.querySelector('.form_type_profile');
-const firstname = document.querySelector('.form__text_type_firstmane');
-const description = document.querySelector('.form__text_type_description');
+export const formTypeProfile = document.querySelector('.form_type_profile');
+export const firstname = document.querySelector('.form__text_type_firstmane');
+export const description = document.querySelector('.form__text_type_description');
 export const profileTitle = document.querySelector('.profile__title');
 export const profileSubtitle = document.querySelector('.profile__subtitle');
 
-function editProfileForm (evt) {
-  evt.preventDefault();
-  profileTitle.textContent = firstname.value;
-  profileSubtitle.textContent = description.value;
-  closePopup (popupTypeProfile);
-}
+// function editProfileForm (evt) {
+//   evt.preventDefault();
+//   profileTitle.textContent = firstname.value;
+//   profileSubtitle.textContent = description.value;
+//   closePopup (popupTypeProfile);
+// }
 
 
 //Вызов закрытие попапов кликом на оверлей
@@ -39,14 +39,6 @@ formTypeProfile.addEventListener('submit', editProfileForm);
 //Вызов валидации форм
 enableValidation ();
 
-//Вызов добавления карточек из массива
-export function addCardsFromArray(element) {
-  element.forEach(function (item) {
-    const card = addCards(item.name, item.link);
-    elementContainer.prepend(card);
-  });
-}
-
 
 //Открытие маодального окна с разными карточками
 const zoomImage = document.querySelector('.popup__zoom-image');
@@ -58,3 +50,58 @@ export function openPopupTypeZoom(title, image) {
   openPopup(popupTypeZoom);
 }
 
+//Загружаем актуальные данные профиля
+function renderResult(title, text) {
+  title.textContent = text;
+}
+
+function renderError(title, err) {
+  title.textContent = err;
+}
+
+function renderAvatar(text) {
+  const avatar = document.querySelector('.profile__image');
+  avatar.src = text;
+}
+
+getProfileInfo()
+.then((res) => {
+  renderResult(profileTitle, res.name);
+  renderResult(profileSubtitle, res.about);
+  renderAvatar(res.avatar);
+})
+.catch((err) => {
+  console.log(`Ошибка: ${err}`);
+  renderError(profileTitle, `Ошибка: ${err}`);
+  renderError(profileSubtitle, `Ошибка: ${err}`);
+});
+
+//Получаем актуальные карточки с сервера
+function addCardsFromArray(element) {
+  element.forEach(function (item) {
+    const card = addCards(item.name, item.link);
+    elementContainer.prepend(card);
+  });
+}
+
+loadCardsFromServer()
+  .then((res) => {
+    addCardsFromArray(res);
+  });
+
+//Отображаем обновленные данные пользователя
+function editProfileForm (evt) {
+  evt.preventDefault();
+  const data = {
+    name: firstname.value,
+    about: description.value
+  }
+  sendProfileInfo(data)
+    .then(res => {
+      renderResult(profileTitle, res.name);
+      renderResult(profileSubtitle, res.about);
+      renderAvatar(res.avatar);
+      closePopup (popupTypeProfile);
+    })
+
+}

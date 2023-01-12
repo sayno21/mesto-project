4b293afbc1999be70f38bbcc116b90e9c8810eae
sayno21 <dallas2112@yandex.ru>
2023-01-12 17:49:08 +0300

    bugfix: validation

--- src/components/validate.js
+++ src/components/validate.js
@@ -25,7 +25,7 @@ const hideError = (formElement, inputElement, inputErrorClass, errorClass) => {
 };
 
 //проверяет на корректность введенных данных и вызывает showError и hideError
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, inputErrorClass, errorClass) => {
   if (inputElement.validity.patternMismatch) {
     //заменяем встроенное сообщение об ошибке
     inputElement.setCustomValidity(inputElement.dataset.errorMessage);
@@ -40,14 +40,14 @@ const checkInputValidity = (formElement, inputElement) => {
 };
 
 //проверяет все поля ввода на валидацию по введенным символам и отключает или включает кнопку
-const setEventListeners = (formElement, inputSelector, submitButtonSelector) => {
+const setEventListeners = (formElement, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass) => {
   const inputList = Array.from(formElement.querySelectorAll(inputSelector));
   const buttonElement = formElement.querySelector(submitButtonSelector);
-  toggleButtonState(inputList, buttonElement);
+  toggleButtonState(inputList, buttonElement, inactiveButtonClass);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
+      checkInputValidity(formElement, inputElement, inputErrorClass, errorClass);
+      toggleButtonState(inputList, buttonElement, inactiveButtonClass);
     });
   });
 }
@@ -89,3 +89,78 @@ export const  stayButtonDidabled = (submitButtonSelector, inactiveButtonClass) =
 }
 
 
+// const showError = (formElement, inputElement, errorMessage) => {
+//   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+//   inputElement.classList.add(settings.inputErrorClass)
+//   errorElement.textContent = errorMessage;
+//   errorElement.classList.add(settings.errorClass);
+// };
+
+// //скрывает сообщение об ошибке
+// const hideError = (formElement, inputElement) => {
+//   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+//   inputElement.classList.remove(settings.inputErrorClass)
+//   errorElement.classList.remove(settings.errorClass);
+//   errorElement.textContent = '';
+// };
+
+// //проверяет на корректность введенных данных и вызывает showError и hideError
+// const checkInputValidity = (formElement, inputElement) => {
+//   if (inputElement.validity.patternMismatch) {
+//     //заменяем встроенное сообщение об ошибке
+//     inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+//   } else {
+//     inputElement.setCustomValidity("");
+//   }
+//   if (!inputElement.validity.valid) {
+//     showError(formElement, inputElement, inputElement.validationMessage)
+//   } else {
+//     hideError(formElement, inputElement)
+//   }
+// };
+
+// //проверяет все поля ввода на валидацию по введенным символам и отключает или включает кнопку
+// const setEventListeners = (formElement) => {
+//   const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
+//   const buttonElement = formElement.querySelector(settings.submitButtonSelector);
+//   toggleButtonState(inputList, buttonElement);
+//   inputList.forEach((inputElement) => {
+//     inputElement.addEventListener('input', function () {
+//       checkInputValidity(formElement, inputElement);
+//       toggleButtonState(inputList, buttonElement);
+//     });
+//   });
+// }
+
+// //валидация всех форм
+// export const enableValidation = () => {
+//   const formList = Array.from(document.querySelectorAll(settings.formSelector));
+//   formList.forEach((formElement) => {
+//     setEventListeners(formElement);
+//   });
+// }
+
+// //проверяет неправильно введенное поле
+// const hasInvalidInput = (inputList) => {
+//   return inputList.some((inputElement) => {
+//   return !inputElement.validity.valid;
+// });
+// }
+
+// //переключатель состояния кнопки
+// const toggleButtonState =  (inputList, buttonElement) => {
+//     if (hasInvalidInput(inputList)) {
+//     buttonElement.classList.add(settings.inactiveButtonClass);
+//     buttonElement.setAttribute('disabled', true);
+//   } else {
+//     buttonElement.classList.remove(settings.inactiveButtonClass);
+//     buttonElement.removeAttribute('disabled', true);
+//   }
+// }
+
+
+// export const  stayButtonDidabled = () => {
+//   const saveButton = popupTypeNewcard.querySelector(settings.submitButtonSelector);
+//   saveButton.classList.add(settings.inactiveButtonClass);
+//   saveButton.setAttribute('disabled', true);
+// }
